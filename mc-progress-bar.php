f9cbb70ae5a8ae2cf2b607885be4b996f618bef9
Joey Nichols <nichols@bermanco.com>
2017-06-07 11:43:23 -0400

    Initial commit

---
+++ mc-progress-bar.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Mc Progress Bar
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     mc-progress-bar
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Mc_Progress_Bar
+ */
+
